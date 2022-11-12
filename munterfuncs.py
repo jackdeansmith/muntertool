b82f4e4b3ceaf9528452790966999d4ff2241b67
Jack Smith <jackdsmith@google.com>
2022-11-11 16:30:52 -0800

    List munter rates

--- munterfuncs.py
+++ munterfuncs.py
@@ -1,12 +1,16 @@
 
-# Gets a time in hours from distance in km, elevation in m, and dimensionless munter rate value
+# Gets a time in hours from distance m, elevation in m, and dimensionless munter rate value
 def munter_forward(distance, elevation, rate):
-    return (distance + elevation/100) / rate
+    return munter_work(distance, elevation) / rate
 
-# Gets a munter rate value from a distance in km, elevation in m, and time in hours
+# Gets a munter rate value from a distance in m, elevation in m, and time
 def munter_reverse(distance, elevation, time):
-    return (distance + elevation/100) / time
+    return munter_work(distance, elevation) / time
 
 # Accepts a distance and elevation and returns a munter work value, this is divided by rate to get time
 def munter_work(distance, elevation):
- return (distance + elevation/100)
+ return ((distance/1000) + abs(elevation)/100)
+
+ # TODO: add sanity checks to these functions
+ # TODO: document properly
+ # TODO: refactor to use timedelta objects for clarity
--- muntertool.py
+++ muntertool.py
@@ -1,6 +1,10 @@
 import click
 import gpxpy
 import gpxpy.gpx
+import gisutil
+import munterfuncs
+
+SECONDS_PER_HOUR = 60 * 60
 
 @click.command()
 @click.argument('gpxfile', type=click.File('r'))
@@ -19,9 +23,66 @@ def muntertool(gpxfile):
         raise click.UsageError("Error: GPX file must contain exactly one track. Found {} tracks.".format(len(gpx.tracks)), ctx=None)
     track = gpx.tracks[0]
 
+    munterstats(track)
+
+def munterstats(track, chunklength=50):
+    """Accepts a gpx track and breaks it into 'chunks' for munter analysis, analyzes each chunk."""
+
+    chunks = []
     for segment in track.segments:
-        for point in segment.points:
-            print('Point at ({0},{1}) -> {2} @{3}'.format(point.latitude, point.longitude, point.elevation, point.time))
+        chunks.extend(chunkify(segment))
+
+    for chunk in chunks: 
+        # print(chunk)
+        munter_rate = munterfuncs.munter_reverse(chunk.distance, chunk.delta_elevation(), chunk.delta_time().total_seconds()/SECONDS_PER_HOUR)
+        print(munter_rate)
+
+class Chunk: 
+    def __init__(self, first_point, last_point, distance):
+        self.first_point = first_point
+        self.last_point = last_point
+        self.distance = distance
+
+    def delta_elevation(self):
+        return self.last_point.elevation - self.first_point.elevation
+
+    def delta_time(self):
+        return self.last_point.time - self.first_point.time
+
+    def __repr__(self):
+        return "Chunk(first point {}, last point {}, distance {}".format(
+            self.first_point,
+            self.last_point,
+            self.distance)
+
+def chunkify(segment, chunklength=50):
+    
+    #TODO: Assert segment has at least two points
+
+    chunks = []
+
+    current_chunk_distance = 0
+    current_chunk_beginning_point = None 
+    current_chunk_last_point = None
+
+    for point in segment.points: 
+        if(current_chunk_beginning_point == None):
+            current_chunk_beginning_point = point 
+            current_chunk_last_point = point 
+            continue 
+
+        current_chunk_distance += gisutil.distance(current_chunk_last_point.longitude, current_chunk_last_point.latitude, point.longitude, point.latitude)
+        current_chunk_last_point = point
+
+        if(current_chunk_distance >= chunklength):
+            chunks.append(Chunk(current_chunk_beginning_point, current_chunk_last_point, current_chunk_distance))
+            current_chunk_distance = 0
+            current_chunk_beginning_point = current_chunk_last_point
+
+    if(current_chunk_distance > 0):
+        chunks.append(Chunk(current_chunk_beginning_point, current_chunk_last_point, current_chunk_distance))
+
+    return chunks
 
 
 if __name__ == '__main__':
